1d8bb1afc6b86b3323f9df55eb703c3d81726f53
cjlworld <cjlworld@outlook.com>
2023-09-10 09:18:55 +0800

    push to remote

--- app/api_bp/schedulers.py
+++ app/api_bp/schedulers.py
@@ -19,16 +19,17 @@ def transform_dict_to_contest(contest_dict: dict):
     )
 
 # 定时任务，每天早上执行一次，获取比赛信息
-@scheduler.task("cron", id='get_contest_info', day="*", hour="2", minute="0", second="0")
+@scheduler.task("cron", id='get_contest_info', day="*", hour="*", minute="0", second="0")
 def get_contest_info():
     # 先检查今天是不是获取过
     if DailyReportModel.query.filter_by(date=str(datetime.date.today())).first() is not None:
         return
     
     daily_report: list[dict] | None = contest_hunter.hunt_all()
-    daily_report.sort(key=(lambda x: x.get('time', '')))
     if daily_report is None:
-        return
+        raise Exception("daily-report failed {:s}".format(str(datetime.datetime.now())))
+    
+    daily_report.sort(key=(lambda x: x.get('time', '')))
     results = list(map(transform_dict_to_contest, daily_report))
     print(datetime.date.today())
     db.session.add(DailyReportModel(date=str(datetime.date.today()), jsons=json.dumps(daily_report)))
